80b586e4d63c8abd82a96e76e38d96d9482057e5
Alexandra <alexmiriamd@gmail.com>
2023-02-07 12:49:10 -0700

    function to mark x or o working

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
-// //setting game state
-
-// const gameState = {
-//     players: ['x', 'o'],
-//     board: [
-//         [null, null, null],
-//         [null, null, null],
-//         [null, null, null]
-//     ]
-// }
-
-// let currentPlayer = gameState.players[0]; //this makes default current player 'x'
-
 
 // variables
+        //buttons and names
 const onePlayerButton = document.getElementById("onePlayer");
 const twoPlayerButton = document.getElementById("twoPlayer");
 let playerOneName = document.getElementById('playerX');
 let playerTwoName = document.getElementById("playerO")
 
-
+        //game play
 let board = document.getElementById("board");
 let cell = document.getElementsByClassName("cells");
+
+const gameState = {           //setting game state object; to keep track of moves, player turns, and wins/ties.
+    players: ['x', 'o'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+}
+
+let currentPlayer = gameState.players[0];       //this makes default current player 'x'; [1] would be 'o'
+
 
 // player amount buttons
 
@@ -37,3 +37,18 @@ twoPlayerButton.addEventListener('click', function(){
     let playerName02 = prompt("Player two name?", ['player two']);
     playerTwoName.innerText = playerName02;
 })
+
+//playing the game.
+
+board.addEventListener('click', placeMark)
+function placeMark(event){
+        if(event.target){
+            const target = event.target;
+            if(currentPlayer === 'x'){
+                target.innerText = 'x';
+            }
+            if(currentPlayer ==='o'){
+                target.innerText = 'o';
+            }
+        }
+    }
