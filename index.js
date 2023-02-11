cbc43a12ea906cd9e98fa14336801f7f5ce54c70
Alexandra <alexmiriamd@gmail.com>
2023-02-10 20:03:02 -0700

    computer player working

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ const gameState = {                             //setting game state object; to 
         [null, null, null],
         [null, null, null],
         [null, null, null]
-    ]
+    ],
+    state: ["gameActive", "gameOver"]
 }
 
 let currentPlayer = gameState.players[0];       //tic-tac-toe rules: player x always first; [1] would be 'o'.
 
+let currentState = gameState.state[1];          // cant start making board until player number chosen
 
 
 //Player amount buttons
@@ -33,6 +35,7 @@ let currentPlayer = gameState.players[0];       //tic-tac-toe rules: player x al
 onePlayerButton.addEventListener('click', function(){
     let playerName = prompt('Player one name?', ['player one']);
     playerOneName.innerText = playerName;
+    currentState = gameState.state[0]
     playerTwoName.innerText = "computer"        ///if playerTwo name is computer run computer player
 })
 
@@ -41,78 +44,91 @@ twoPlayerButton.addEventListener('click', function(){
     playerOneName.innerText = playerName01;
     let playerName02 = prompt("Player two name?", ['player two']);
     playerTwoName.innerText = playerName02;
+    currentState = gameState.state[0];
 })
 
 
-function computerPlayer(){                          //invoked in the turnChange function
-    if(playerTwoName.innerText === "computer"){
+function computerPlayer(){                          //computer playing as 'o'
         console.log('are we running this func?')
-        let target = randomCellFunc();
 
-        if(target === document.getElementById("cell1").id && document.getElementById("cell1").innerText === ''){
-            document.getElementById("cell1").innerText = 'o';
-        } else
-        if(target === document.getElementById("cell2").id && document.getElementById("cell2").innerText === ''){
-            document.getElementById("cell2").innerText = 'o';
-        } else
-        if(target === document.getElementById("cell3").id && document.getElementById("cell3").innerText === ''){
-            document.getElementById("cell3").innerText = 'o';
-        } else
-        if(target === document.getElementById("cell4").id && document.getElementById("cell4").innerText === ''){
-            document.getElementById("cell4").innerText = 'o';
-        } else
-        if(target === document.getElementById("cell5").id && document.getElementById("cell5").innerText === ''){
+        if(document.getElementById("cell5").innerText === ''){
             document.getElementById("cell5").innerText = 'o';
-        } else 
-        if(target === document.getElementById("cell6").id && document.getElementById("cell6").innerText === ''){
-            document.getElementById("cell6").innerText = 'o';
+            updateGameBoard(document.getElementById("cell5"))
         } else
-        if(target === document.getElementById("cell7").id && document.getElementById("cell7").innerText === ''){
+        if(document.getElementById("cell1").innerText === ''){
+            document.getElementById("cell1").innerText = 'o';
+            updateGameBoard(document.getElementById("cell1"));
+        } else
+        if(document.getElementById("cell3").innerText === ''){
+            document.getElementById("cell3").innerText = 'o';
+            updateGameBoard(document.getElementById("cell3"));
+        } else
+        if(document.getElementById("cell7").innerText === ''){
             document.getElementById("cell7").innerText = 'o';
+            updateGameBoard(document.getElementById("cell7"));
         } else
-        if(target === document.getElementById("cell8").id && document.getElementById("cell8").innerText === ''){
-            document.getElementById("cell8").innerText = 'o';
-        } else
-        if(target === document.getElementById("cell9").id && document.getElementById("cell9").innerText === ''){
+        if(document.getElementById("cell9").innerText === ''){
             document.getElementById("cell9").innerText = 'o';
-        } else{ 
-            computerPlayer();
+            updateGameBoard(document.getElementById("cell9"));
+        } else
+        if(document.getElementById("cell2").innerText === ''){
+            document.getElementById("cell2").innerText = 'o';
+            updateGameBoard(document.getElementById("cell2"));
+        } else
+        if(document.getElementById("cell6").innerText === ''){
+            document.getElementById("cell6").innerText = 'o';
+            updateGameBoard(document.getElementById("cell6"));
+        } else
+        if(document.getElementById("cell4").innerText === ''){
+            document.getElementById("cell4").innerText = 'o';
+            updateGameBoard(document.getElementById("cell4"));
+        } else
+        if(document.getElementById("cell8").innerText === ''){
+            document.getElementById("cell8").innerText = 'o';
+            updateGameBoard(document.getElementById("cell8"));
         }
-    }
-    turnChange();
 }
 
-function randomCellFunc(){                                      //returns random cell number 1-9 for computerPlayer
-    let randomNum = Math.floor(Math.random() * 9) +1;
-    let randomCell = `cell${randomNum}`
-    return randomCell
-}
+
+
+
+// function randomCellFunc(){                                      //returns random cell number 1-9 for computerPlayer
+//     let randomNum = Math.floor(Math.random() * 9) +1;
+//     let randomCell = `cell${randomNum}`
+//     return randomCell
+// }
 
 
 
 //playing the game.
 
 board.addEventListener('click', placeMark)
-    function placeMark(event){                        
-        if(event.target){
-            const target = event.target;
-            if(currentPlayer === 'x' && target.innerText === ''){
-                target.innerText = 'x';
-                updateGameBoard(target);
-                turnChange();
-            } else 
-            if(target.innerText === ''){
-                target.innerText = 'o';
-                updateGameBoard(target);
-                turnChange();
+    function placeMark(event){  
+        if(currentState === gameState.state[0]){                      
+            if(event.target){
+                const target = event.target;
+                if(currentPlayer === 'x' && target.innerText === ''){
+                    target.innerText = 'x';
+                    updateGameBoard(target);
+                    turnChange();
+                } else 
+                if(target.innerText === ''){
+                    target.innerText = 'o';
+                    updateGameBoard(target);
+                    turnChange();
+                }
             }
         }
     }
 
 function turnChange(){
-    if(currentPlayer === 'x'){
+    if(currentPlayer === 'x' && currentState === gameState.state[0]){
+
         currentPlayer = gameState.players[1];
-        computerPlayer();
+        if(playerTwoName.innerText === "computer"){
+            computerPlayer();
+            currentPlayer = gameState.players[0];
+        }
     } else {
         currentPlayer = gameState.players[0];
     }
@@ -121,36 +137,29 @@ function turnChange(){
 function updateGameBoard(target){                //reflecting the turns to the gameBoard array
     if(target.id === "cell1"){
         gameState.board[0][0] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell2"){              
         gameState.board[0][1] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell3"){
         gameState.board[0][2] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell4"){
         gameState.board[1][0] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell5"){
         gameState.board[1][1] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell6"){
         gameState.board[1][2] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell7"){
         gameState.board[2][0] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell8"){
         gameState.board[2][1] = currentPlayer;
-        checkForWin();
     } else if(target.id === "cell9"){
         gameState.board[2][2] = currentPlayer;
-        checkForWin();
     }
+    checkForWin();
 }
 
 function announceWinner(){
-    alert(`yay!! ${currentPlayer} wins!`);
+    setTimeout(alert(`yay!! ${currentPlayer} wins!`), 3000);
+    currentState = gameState.state[1];
 }
 
 function checkForWin(){                     //checking for all wins, invoked in updateGameBoard function
@@ -185,7 +194,7 @@ function checkForTie(){                 //checking for tie if no win is logged
     if(gameState.board[0].includes(null) || gameState.board[1].includes(null) || gameState.board[2].includes(null)){
         console.log("game continues")
     } else {
-        alert("oh no!! there has been a tie.") //maybe put a confirm box to ask, reset game? instead of alert. still need reset button available overall tho. 
+        alert("oh no!! there has been a tie.") //maybe put a confirm box to ask, reset game? instead of alert.
     }
 }
 
@@ -212,5 +221,6 @@ function restartGame(){                 //putting game back to original gameStat
     document.getElementById("cell9").innerText = "";
 
     currentPlayer = gameState.players[0];
+    currentState = gameState.state[0];
     console.log(gameState.board);
 }
